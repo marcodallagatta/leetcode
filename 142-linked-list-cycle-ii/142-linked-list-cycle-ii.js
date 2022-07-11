5bb26f1b3bcf0cdcb9ba141fa72fb6ddac68d71c
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2022-07-11 23:11:27 +0200

    Time: 96 ms (72.48%), Space: 45.2 MB (35.88%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -12,7 +12,6 @@
  */
 var detectCycle = function(head) {
 
-  console.log('-/-/-/-')  
   let slow = head;
   let fast = head;
   if (!slow || !slow.next) return null;
@@ -23,23 +22,14 @@ var detectCycle = function(head) {
     fast = fast.next.next;
     if (!fast || !fast.next) return null;
     if (slow === fast && slow) break;
-    console.log('slow ' + slow.val);
-    console.log('fast ' + fast.val);
-    console.log('- - -');
   }
-  console.log('uscito');
   slow = head;
-  console.log('slow ' + slow.val);
-  console.log('- - -');
   
   // checks which node the loop starts again from
   while (fast) {
     if (slow === fast) return slow;
     slow = slow.next;
     fast = fast.next;
-    console.log('slow ' + slow.val);
-    console.log('fast ' + fast.val);
-    console.log('- - -');
   }
   
 };
