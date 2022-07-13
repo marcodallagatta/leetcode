4431ac4d722d420acacfe9490d2d42aea6543691
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2022-07-14 00:13:35 +0200

    Time: 98 ms (33.18%), Space: 42.2 MB (38.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -5,19 +5,14 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
+var merge = function (nums1, m, nums2, n) {
   nums1.splice(m);
   nums2.splice(n);
 
   let checkStart = 0;
-  
+
   while (nums2.length) {
-   
     for (let i = checkStart; i <= nums1.length; i++) {
-      // console.log('i is: ' + i + ' nums1.length is: ' + nums1.length);
-      // console.log(nums1);
-      // console.log(nums2);
-      
       if (i === nums1.length) {
         nums1.splice(nums1.length, 0, nums2[0]);
         break;
@@ -29,9 +24,5 @@ var merge = function(nums1, m, nums2, n) {
     }
     checkStart++;
     nums2.shift();
-    // console.log(' - - - ')
-    // console.log(nums1);
-    // console.log(nums2);
   }
-  
 };
