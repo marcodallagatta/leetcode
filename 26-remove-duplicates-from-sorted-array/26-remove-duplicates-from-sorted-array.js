0117ccd3b83a70dfac56f46fb60cf6974d35427e
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2022-07-14 12:46:42 +0200

    Time: 300 ms (7.03%), Space: 49 MB (5.31%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let currentCheck = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== currentCheck) {
+      currentCheck = nums[i];
+    } else  {
+      nums.splice(i, 1).push('_');
+      i--;
+    }
+  }
+};
