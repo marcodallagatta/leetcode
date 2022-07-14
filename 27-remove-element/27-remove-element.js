fd1324597e8d55e284415df3ba15b358ded4268f
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2022-07-14 12:40:06 +0200

    Time: 62 ms (94.09%), Space: 41.8 MB (77.51%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -4,11 +4,9 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-  let numsLength = nums.length;
-  for (let i = 0; i < numsLength; i++) {
+  for (let i = 0; i < nums.length; i++) {
     if (nums[i] === val) {
       nums.splice(i, 1).push('_');
-      numsLength--;
       i--;
     }
   }
