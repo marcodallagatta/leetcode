560582a255c50b47f26cc52755779e3ceef7216c
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2024-02-20 23:42:58 +0100

    Time: 96 ms (28.99%) | Memory: 68.2 MB (5.12%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -4,8 +4,5 @@
  */
 var containsDuplicate = function(nums) {
     const dup = new Set([...nums]);
-    console.log(JSON.stringify(nums))
-    console.log(JSON.stringify([...dup]))
-    console.log(JSON.stringify(nums) === JSON.stringify([...dup]))
     return JSON.stringify(nums) === JSON.stringify([...dup]) ? false : true;
 };
