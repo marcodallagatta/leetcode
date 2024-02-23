c3827c91b294cfab7032c393305e87a41a57c166
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2024-02-23 22:23:15 +0100

    Time: 134 ms (16.96%) | Memory: 49.4 MB (44.96%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -7,11 +7,7 @@ var twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         const diff = target - nums[i];
         const ind = nums.indexOf(diff);
-        console.log(`i is ${i}, diff is ${diff}, ind is ${ind}, ${ind !== -1 && ind !== i}`)
-        if (ind !== -1 && ind !== i) {
-            console.log([i, ind])
-            return [i, ind]
-        }
+        if (ind !== -1 && ind !== i) return [i, ind]
         if (i === nums[i]) continue
     } 
 };
