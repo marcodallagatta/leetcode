69e752ae6fec7c7728203d52947b13ce06eb6ebc
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2024-02-23 22:50:24 +0100

    Time: 86 ms (27.41%) | Memory: 54.5 MB (11.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,6 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    if (s.length !== t.length) return false;
     return JSON.stringify([...s].sort()) === JSON.stringify([...t].sort());
 };
