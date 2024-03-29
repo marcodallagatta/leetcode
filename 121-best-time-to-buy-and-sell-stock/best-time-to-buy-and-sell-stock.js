5c5816719936f559019b5e05852f50a101cc0ea8
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2024-03-29 21:59:12 +0100

    Time: 88 ms (9.50%) | Memory: 58.9 MB (62.90%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -6,13 +6,13 @@ var maxProfit = function (prices) {
   let left = prices[0];
   let right = prices[1];
   let maxProfit = right - left;
-  console.log(left, right, maxProfit);
+//   console.log(left, right, maxProfit);
 
   for (let i = 1; i < prices.length - 1; i++) {
     if (prices[i] < left) left = prices[i];
     right = prices[i + 1];
     maxProfit = right - left > maxProfit ? right - left : maxProfit;
-    console.log(left, right, maxProfit);
+    // console.log(left, right, maxProfit);
   }
 
   return maxProfit > 0 ? maxProfit : 0;
